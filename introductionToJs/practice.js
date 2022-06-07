e36989be1473e7ed4a3666a5e3933a6d7fa49fa9
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-07 18:42:29 +0530

    declaration , initilization and print

--- introductionToJs/practice.js
+++ introductionToJs/practice.js
@@ -2,3 +2,13 @@ console.log("hi there!!!")  // it's just to prints out hi there
 /*
  multiline comments
 */
+var a;
+console.log(a); // it will give undefined output
+a = 10;
+console.log(a);// it will give 10 as output
+var b = "ravi";
+console.log(b); // it will give ravi as output
+console.log(1+1); // it will give 2 as output
+console.log(1+1+"1"); //it will give 21 as output
+console.log('1'+1+1);//it will give 111 as output
+console.log(1-"1");//it will give 0 as output
