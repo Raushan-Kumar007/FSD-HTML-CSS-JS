df43083ff85f1d40777f285ee7eda44ecd10fa8a
Rausha Kumar <ravisankaranand199@gmail.com>
2022-06-10 18:34:24 +0530

    index.html

---
+++ introductionToJs/index.js
@@ -0,0 +1,23 @@
+// console.log(a);
+// console.log(x());
+ 
+//  var a = 100;
+//  function x(){
+//     return 10;
+//  }
+  
+console.log(sum());
+console.log(sum1);
+console.log(sum2);
+
+function sum (){
+    return 2+3;
+}
+var sum1 = function(){
+    return 2+3;
+};
+// arrow function 
+const sum2 = () => {
+    return 2 + 3
+};
+ 
